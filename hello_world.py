45053304a50f874d580505c92aa482a85a08a57c
Jungsooan <sooan1022@gmail.com>
2023-09-13 15:22:15 +0900

    sam

--- hello_world.py
+++ hello_world.py
@@ -4,7 +4,11 @@ def print_hello():
 def print_ Kit():
     print("Kumoh")
 
+def print_sam():
+    print("sooan")
+
 if __name__ == '__main__':
     print_hello()
     print_Kit()
+    print_sam()
     
