b0131302df21c36b42d3f5c86fe913a49593af97
Jungsooan <sooan1022@gmail.com>
2023-09-14 14:16:00 +0900

    commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import max_profit as mp
 import prime_number as pn
 
 if __name__ == '__main__':
-    pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
+    pn.is_prime_number(21)
+    pn.is_prime_number(113)
+    pn.is_prime_number(124)
+    pn.is_prime_number(101)
     
